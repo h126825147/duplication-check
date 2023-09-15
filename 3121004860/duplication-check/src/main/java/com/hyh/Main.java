1260347db4a077166426af6f074df5ca5f596de4
HYHAO <1270457859@qq.com>
2023-09-15 10:56:09 +0800

    feat:完成文件读取和写入功能 chore:导入JUnit和Logback依赖

--- 3121004860/duplication-check/src/main/java/com/hyh/Main.java
+++ 3121004860/duplication-check/src/main/java/com/hyh/Main.java
@@ -1,7 +1,12 @@
 package com.hyh;
 
+import com.hyh.util.IOUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String orig = "D:\\HYHAO\\Documents\\duplication-check\\3121004860\\duplication-check\\src\\main\\java\\orig.txt";
+        String target = "D:\\HYHAO\\Documents\\duplication-check\\3121004860\\duplication-check\\src\\main\\java\\target.txt";
+        System.out.println(IOUtils.readFileContent(orig));
+        IOUtils.writeFileContent(target, "hello");
     }
 }
---
+++ 3121004860/duplication-check/src/main/java/com/hyh/util/IOUtils.java
@@ -0,0 +1,39 @@
+package com.hyh.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+public class IOUtils {
+    public static final Logger LOGGER = LoggerFactory.getLogger("Main.class");
+    private IOUtils() {}
+
+    /**
+     * 读取文件内容
+     * @param filePath  文件路径
+     * @return  文件内容
+     */
+    public static String readFileContent(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            LOGGER.error("文件读取失败...");
+        }
+        return content.toString();
+    }
+
+    public static void writeFileContent(String filePath, String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            LOGGER.error("文件读取失败...");
+        }
+    }
+}
